1e14027442eb925aabcc255da692637e3f6642a1
reza <moradzade.mohamadreza@gmail.com>
2025-07-24 21:53:09 -0700

    forth commit

---
+++ lib/car.dart
@@ -0,0 +1,7 @@
+class Car {
+  String name;
+  String topSpeed;
+  String price;
+
+  Car({required this.name, required this.topSpeed, required this.price});
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String inputText = '';
+  var controller = TextEditingController();
+  var box = Hive.box('names');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(controller: controller),
+            Text(box.get(2) ?? 'empty', style: const TextStyle(fontSize: 30)),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(fontSize: 26),
+              ),
+              onPressed: () {
+                box.put(2, controller.text);
+                setState(() {});
+              },
+              child: Text('read from textfiled'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(fontSize: 26),
+              ),
+              onPressed: () {
+                box.put(1, 'amir');
+              },
+              child: Text('create'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(fontSize: 26),
+              ),
+              onPressed: () {
+                var text = box.get(1);
+                print(text);
+              },
+              child: Text('read'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(fontSize: 26),
+              ),
+              onPressed: () {
+                box.put(1, 'Ariana');
+              },
+              child: Text('update'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(fontSize: 26),
+              ),
+              onPressed: () {
+                box.delete(1);
+              },
+              child: Text('delete'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
